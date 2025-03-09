9c39cfdff5478f93c8930b2c31b41fb05378a7a5
AlyssonP <alysson.nascimento@polodeinovacao.ifpb.edu.br>
2025-03-09 13:57:03 -0300

    Criando Api historia

---
+++ src/main/java/com/example/api_historia/ApiHistoriaApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_historia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiHistoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiHistoriaApplication.class, args);
+	}
+
+}
