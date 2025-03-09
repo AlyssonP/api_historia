73fe2e99a58d376694c51843bf492cfe99dfb3fd
AlyssonP <alysson.nascimento@polodeinovacao.ifpb.edu.br>
2025-03-09 14:01:28 -0300

    Geração da classe test

---
+++ src/test/java/com/example/api_historia/ApiHistoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_historia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiHistoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
